0b8955e0275735a92ad3d3899f5e4a291eb316a2
StijnVM <stijn.vm@hotmail.com>
2025-04-03 11:07:37 +0200

    Fixed SQL injection using parameterized queries

--- server.js
+++ server.js
@@ -15,12 +15,31 @@ app.post("/login", async (req, res) => {
 	const { username, password } = req.body;
 
 	// Direct injection of user input into raw SQL query:
-	const user = await prisma.$queryRawUnsafe(`SELECT * FROM User WHERE username = '${username}' AND password = '${password}'`);
+	// const user = await prisma.$queryRawUnsafe(`SELECT * FROM User WHERE username = '${username}' AND password = '${password}'`);
 
-	if (user.length > 0) {
-		res.json({ message: "Login successful!", user });
-	} else {
-		res.status(401).json({ message: "Invalid credentials" });
+	try {
+		// Solution 1: Raw SQL with parameterization
+		const user = await prisma.$queryRaw`
+			SELECT * FROM User WHERE username = ${username} AND password = ${password}
+		`;
+
+		// Solution 2: Prisma ORM
+		// const user2 = await prisma.user.findUnique({
+		// 	where: { username },
+		// });
+		// if (user2 && user2.password === password) {
+		// 	res.json({ message: "Login successful", user2 });
+		// } else {
+		// 	res.status(401).json({ message: "Invalid credentials" });
+		// }
+
+		if (user.length > 0) {
+			res.json({ message: "Login successful!", user });
+		} else {
+			res.status(401).json({ message: "Invalid credentials" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: "Internal server error", error });
 	}
 });
 
